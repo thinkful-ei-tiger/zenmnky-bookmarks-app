681ce724d1730c015506858a973ce4fc8748a2d8
Justin Hager <coachjustinhager@gmail.com>
2020-10-30 12:40:22 -0700

    production build. removed hard coded bookmarks

--- src/js/store.js
+++ src/js/store.js
@@ -3,24 +3,7 @@ import api from './api';
 import bookmarkFunctions from './bookmarkList';
 
 const store = {
-    bookmarks: [
-      {
-        id: 'x56w',
-        title: 'Title 1',
-        rating: 3,
-        url: 'http://www.title1.com',
-        description: 'lorem ipsum dolor sit',
-        expanded: false
-      },
-      {
-        id: '6ffw',
-        title: 'Title 2',
-        rating: 5,
-        url: 'http://www.title2.com',
-        description: 'dolorum tempore deserunt',
-        expanded: false
-      } 
-    ],
+    bookmarks: [],
     adding: false,
     error: null,
     filter: 0
